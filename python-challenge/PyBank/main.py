10a19854bcb4f385e4343df120586a0b41183282
alee1412 <alee1412@gmail.com>
2018-05-12 01:11:01 -0700

    update

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -7,52 +7,66 @@ filepath = os.path.join("budget_data_1.csv")
 with open(filepath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    months = []
+    total_months = []
 
     for row in csvreader:
-        months.append(row[0]) 
+        total_months.append(row[0]) 
 
-    #print(months)
-    a = (len(months) - 1)
-    print(a)
+    months = (len(total_months) - 1)
+    print(months)
 
 #Total Revenue
 with open(filepath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    amount = 0
+    total_revenue = 0
 
-    for total in csvreader:
-        if total[1] != "Revenue":
-            amount = amount + int(total[1])
-    print(amount)
+    for revenue in csvreader:
+        if revenue[1] != "Revenue":
+            total_revenue = total_revenue + int(revenue[1])
+    print(total_revenue)
 
 #Average Change in revenue
 with open(filepath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    change = []
+    average_change = []
 
     for row in csvreader:
-        change.append(row[1])
-    change.pop(0)
-    change = list(map(int, change))
-    print (change)
+        average_change.append(row[1])
+    average_change.pop(0)
+    average_change = list(map(int, average_change))
+    #print (average_change)
     
-    new = []
-    j = 0
-    for items in range(len(change)-1):
-        new.append(change[j + 1] - change[j])
-        j = j + 1
-    print(new)
+    new_change = []
+    change = 0
+    
+    for items in range(len(average_change)-1):
+        new_change.append(average_change[change + 1] - average_change[change])
+        change = change + 1
+    #print(new_change)
 
-    average_change = []
-    average_change = new/a
-    print(average_change)
+    average_monthly_change = sum(new_change)/len(new_change)
+    print(average_monthly_change)
+
 #Greatest increase in revenue (date and month)
-#with open(filepath, newline="") as csvfile:
-   # csvreader = csv.reader(csvfile, delimiter=",")
+
+    #print(new_change)
+    i = len(new_change)
+
+    def largest(new_change, i):
+        return max(new_change)
+
+    print(largest(new_change, i))
+
+
 
 #Greatest decrease in revenue (date and month)
-#with open(filepath, newline="") as csvfile:
-    #csvreader = csv.reader(csvfile, delimiter=",")
+    
+    #print(new_change)
+    d = len(new_change)
+
+    def lowest(new_change, d):
+        return min(new_change)
+
+    print(lowest(new_change, d))
--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -13,3 +13,12 @@ with open(filepath, newline="") as csvfile:
         total_votes.append(row[0]) 
 
     print(len(total_votes) - 1)
+
+#List of candidates
+
+    candidates = []
+
+    for row in csvreader:
+        candidates.pop(0)
+        candidates.append(row[2])
+        candidates.pop(0)
